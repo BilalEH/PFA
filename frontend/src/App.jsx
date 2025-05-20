bc5f6a09cd5e85133dabfdb4ca81fc03a07e8c49
AHMEDNAHRI <ahmedphone2004@gmail.com>
2025-05-20 12:56:42 +0100

    bilal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import {RouterProvider } from 'react-router-dom';
 import './styles/authStyles.css';
-import { router } from "./routes";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="*" element={<Login />} />
-    </Routes>
+    test
   );
 }
 
